15af16f9e8f063e6248d44f10bdb0e3257959239
flashmaestro <masterofflash@nate.com>
2018-09-20 15:12:34 +0900

    first onlineshop commit

---
+++ shop/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from .models import *
+# Register your models here.
+# 옵션 클래스 만들고 등록
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','slug','category','price','stock','available','created','updated']
+    list_filter = ['available','created','updated','category']
+    prepopulated_fields = {'slug': ('name',)}
+    list_editable = ['price','stock','available']
+
+admin.site.register(Product, ProductAdmin)
